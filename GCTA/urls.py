61f73448811d41bfec5523207877ce58a6844bbd
harish <harish@kavinsoft.com>
2021-11-27 09:48:40 +0530

    Session insertions with migrations

--- GCTA/urls.py
+++ GCTA/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index , name='index'),
     path('HomePage', views.HomePage , name='HomePage'),
+    path('Login', views.Login , name='Login'),
     path('RegisterPage', views.RegisterPage , name='RegisterPage'),
     path('Submission', views.Submission , name='Submission'),
     path('Forgotpassword', views.Forgotpassword , name='Forgotpassword'),
-    path('Consent', views.Consent , name='Consent'),
     path('Pre_and_post', views.Pre_and_post , name='Pre_and_post'),
+    path('Consent', views.Consent , name='Consent'),
 ]
 
--- GCTA/views.py
+++ GCTA/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-# from django.contrib.auth.models import user,auth
+
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.session.has_key('username'):
+        # username = request.session['username']
+        return redirect('HomePage')
+    else:
+      return render(request,'index.html')
+
+def Login(request):
+    if request.method == 'GET':
+        UserId = request.GET['userid']
+        request.session['username'] = UserId
+        return HttpResponse('success')
 
 def HomePage(request):
+        # username = request.session['username']
     return render(request,'HomePage.html')
 
 def RegisterPage(request):
